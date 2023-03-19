bab3f6ed33ff60c2eec62402980c99e6da13d8bb
Nachuan Wang <1187014311@qq.com>
2023-03-19 01:36:59 -0500

    Lab 6 linear search part finished

--- labs/triangle_counting/template.cu
+++ labs/triangle_counting/template.cu
@@ -4,6 +4,46 @@
 
 #include "template.hu"
 
+__device__ static uint64_t linear_search(
+  const uint32_t *const edgeDst,
+  uint32_t uPtr,
+  uint32_t uEnd,
+  uint32_t vPtr,
+  uint32_t vEnd
+) {
+  uint64_t count = 0;
+  uint32_t w1 = edgeDst[uPtr];
+  uint32_t w2 = edgeDst[vPtr];
+
+  while (uPtr < uEnd && vPtr < vEnd) {
+    if (w1 < w2) {
+      w1 = edgeDst[++uPtr];
+    } else if (w1 > w2) {
+      w2 = edgeDst[++vPtr];
+    } else {
+      w1 = edgeDst[++uPtr];
+      w2 = edgeDst[++vPtr];
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// __device__ static uint64_t binary_search(
+//   const uint32_t *const edgeDst,
+//   uint32_t uPtr,
+//   uint32_t uEnd,
+//   uint32_t vPtr,
+//   uint32_t vEnd
+// ) {
+//   uint64_t count = 0;
+//   uint32_t w1 = edgeDst[uPtr];
+//   uint32_t w2 = edgeDst[vPtr];
+
+//   return count;
+// }
+
 __global__ static void kernel_tc(uint64_t *__restrict__ triangleCounts, //!< per-edge triangle counts
                                  const uint32_t *const edgeSrc,         //!< node ids for edge srcs
                                  const uint32_t *const edgeDst,         //!< node ids for edge dsts
@@ -12,32 +52,45 @@ __global__ static void kernel_tc(uint64_t *__restrict__ triangleCounts, //!< per
 ) {
 
   // Determine the source and destination node for the edge
+  size_t idx = blockDim.x * blockIdx.x + threadIdx.x;
 
-  // Use the row pointer array to determine the start and end of the neighbor list in the column index array
+  if (idx < numEdges) {
+    // Use the row pointer array to determine the start and end of the neighbor list in the column index array
+    uint32_t srcNode = edgeSrc[idx];
+    uint32_t dstNode = edgeDst[idx];
+    uint32_t uPtr = rowPtr[srcNode];
+    uint32_t uEnd = rowPtr[srcNode+1];
+    uint32_t vPtr = rowPtr[dstNode];
+    uint32_t vEnd = rowPtr[dstNode+1];
 
-  // Determine how many elements of those two arrays are common
+    // Determine how many elements of those two arrays are common
+    triangleCounts[idx] = linear_search(edgeDst, uPtr, uEnd, vPtr, vEnd);
+  }
 }
 
 uint64_t count_triangles(const pangolin::COOView<uint32_t> view, const int mode) {
   //@@ create a pangolin::Vector (uint64_t) to hold per-edge triangle counts
   // Pangolin is backed by CUDA so you do not need to explicitly copy data between host and device.
   // You may find pangolin::Vector::data() function useful to get a pointer for your kernel to use.
+  pangolin::Vector<uint64_t> tc(view.nnz(), 0);
 
   uint64_t total = 0;
 
   dim3 dimBlock(512);
   //@@ calculate the number of blocks needed
-  // dim3 dimGrid (ceil(number of non-zeros / dimBlock.x))
+  dim3 dimGrid(ceil(view.nnz()/ (float)dimBlock.x));
 
   if (mode == 1) {
 
     //@@ launch the linear search kernel here
-    // kernel_tc<<<dimGrid, dimBlock>>>(...)
+    kernel_tc<<<dimGrid, dimBlock>>>(tc.data(), view.row_ind(), view.col_ind(), view.row_ptr(), view.nnz());
+    cudaDeviceSynchronize();
 
   } else if (2 == mode) {
 
     //@@ launch the hybrid search kernel here
     // your_kernel_name_goes_here<<<dimGrid, dimBlock>>>(...)
+    // cudaDeviceSynchronize();
 
   } else {
     assert("Unexpected mode");
@@ -45,5 +98,8 @@ uint64_t count_triangles(const pangolin::COOView<uint32_t> view, const int mode)
   }
 
   //@@ do a global reduction (on CPU or GPU) to produce the final triangle count
+  for (int i = 0; i < view.nnz(); i++) {
+    total += tc[i];
+  }
   return total;
 }
