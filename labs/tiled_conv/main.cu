458b9c4cae9fb60ee02380a74b707c0f4eecc2e0
Nachuan Wang <1187014311@qq.com>
2023-03-27 10:22:40 -0500

    Lab 7 buggy version

--- labs/tiled_conv/main.cu
+++ labs/tiled_conv/main.cu
@@ -1,5 +1,10 @@
 #include "helper.hpp"
 
+#define TILE_SZ_A 128
+#define TILE_SZ_B 16
+#define TILE_SZ_RATIO (TILE_SZ_A/TILE_SZ_B)
+
+__constant__ float const_k[32 * 1 * 5 * 5];
 
 // Sequential code for the forward path of the convolution layer
 // You should not modify this code
@@ -78,12 +83,104 @@ static void convlayer_gpu_baseline(const float *X, const shape &xdims, const flo
 __global__ void conv_forward_opt_kernel(const float *X, const shape xdims, const float *W, const shape wdims, float *Y,
   const shape ydims) {
 
-    const size_t gx = blockIdx.x * blockDim.x + threadIdx.x;
-  for (size_t i = gx; i < ydims.num * ydims.depth * ydims.height * ydims.width; i += blockDim.x * gridDim.x) {
-    Y[i] = 0.f;
+  //@@ YOUR CODE HERE!
+  
+  // Virtualize input/weight as matrix
+  const int b = blockIdx.z;
+  const int M = ydims.depth;
+  const int C1 = xdims.depth;
+  const int K = wdims.height;
+  const int H = ydims.height;
+  const int W1 = ydims.width;
+  const int H_out = H - K + 1;
+  const int W_out = W1 - K + 1;
+  const int K_size = K * K;
+
+  #define y4d_mtx(ri, ci) Y[(b) * (M * H_out * W_out) + (ri) * (H_out * W_out) + ((ci) / W_out) * (W_out) + ((ci) % W_out)]
+  #define x4d_mtx(ri, ci) X[(b) * (C1 * H * W1) + ((ri) / K_size) * (H * W1) + (((ri) % K_size) / K + ((ci) / W_out)) * (W1) + (((ri) % K_size) % K + ((ci) % W_out))]
+  #define k4d_mtx(ri, ci) const_k[(ri) * (C1 * K * K) + (ci)]
+
+  const int m = M;
+  const int k = K_size * C1;
+  const int n = H_out * W_out;
+
+  #define A(row, col) k4d_mtx((col), (row))
+  #define B(row, col) x4d_mtx((row), (col))
+  #define C(row, col) y4d_mtx((col), (row))
+
+  // Do the normal tiled matrix multiplication
+  __shared__ float B_ds[TILE_SZ_RATIO][TILE_SZ_B];
+  int ti = threadIdx.x;     // Thread index
+  int row = blockIdx.x * TILE_SZ_A + ti;
+  int col = blockIdx.y * TILE_SZ_B;
+  float out0 = 0, out1 = 0, out2 = 0, out3 = 0, out4 = 0, out5 = 0, out6 = 0, out7 = 0,
+    out8 = 0, out9 = 0, out10 = 0, out11 = 0, out12 = 0, out13 = 0, out14 = 0, out15 = 0;
+  
+  for (int tn = 0; tn < ceil((float) k / TILE_SZ_RATIO); tn++) {
+    // Load shared memory
+    int rowS = ti / TILE_SZ_B;
+    int colS = ti % TILE_SZ_B;
+    int rowB = tn * TILE_SZ_RATIO + rowS;
+    int colB = col + colS;
+    if (rowB < k && colB < n) {
+      B_ds[rowS][colS] = B(rowB, colB);
+    } else {
+      B_ds[rowS][colS] = 0;
+    }
+    __syncthreads();
+
+    // Load register & Accumulate result
+    for (int i = 0; i < TILE_SZ_RATIO; i++) {
+      if (row < m && tn * TILE_SZ_RATIO < k) {
+        float Ai = A(row, tn * TILE_SZ_RATIO + i);
+        out0 += Ai * B_ds[i][0];
+        out1 += Ai * B_ds[i][1];
+        out2 += Ai * B_ds[i][2];
+        out3 += Ai * B_ds[i][3];
+        out4 += Ai * B_ds[i][4];
+        out5 += Ai * B_ds[i][5];
+        out6 += Ai * B_ds[i][6];
+        out7 += Ai * B_ds[i][7];
+        out8 += Ai * B_ds[i][8];
+        out9 += Ai * B_ds[i][9];
+        out10 += Ai * B_ds[i][10];
+        out11 += Ai * B_ds[i][11];
+        out12 += Ai * B_ds[i][12];
+        out13 += Ai * B_ds[i][13];
+        out14 += Ai * B_ds[i][14];
+        out15 += Ai * B_ds[i][15];
+      }
+    }
+    __syncthreads();
   }
 
-  //@@ YOUR CODE HERE!
+  // Write result
+  if (row < m) {
+    if (col < n) C(row, col) = out0;
+    if (col + 1 < n) C(row, col + 1) = out1;
+    if (col + 2 < n) C(row, col + 2) = out2;
+    if (col + 3 < n) C(row, col + 3) = out3;
+    if (col + 4 < n) C(row, col + 4) = out4;
+    if (col + 5 < n) C(row, col + 5) = out5;
+    if (col + 6 < n) C(row, col + 6) = out6;
+    if (col + 7 < n) C(row, col + 7) = out7;
+    if (col + 8 < n) C(row, col + 8) = out8;
+    if (col + 9 < n) C(row, col + 9) = out9;
+    if (col + 10 < n) C(row, col + 10) = out10;
+    if (col + 11 < n) C(row, col + 11) = out11;
+    if (col + 12 < n) C(row, col + 12) = out12;
+    if (col + 13 < n) C(row, col + 13) = out13;
+    if (col + 14 < n) C(row, col + 14) = out14;
+    if (col + 15 < n) C(row, col + 15) = out15;
+  }
+
+  #undef A
+  #undef B
+  #undef C
+
+  #undef y4d_mtx
+  #undef x4d_mtx
+  #undef k4d_mtx
 }
 
 // Host code to configure baseline GPU kernel
@@ -92,8 +189,13 @@ static void convlayer_gpu_opt(const float *X, const shape &xdims, const float *W
 
   // Modify this code to configure your optimized kernel.
   //@@ YOUR CODE HERE!!!
-  dim3 dimGrid(1);
-  dim3 dimBlock(32);
+  int H_out = ydims.height - wdims.height + 1;
+  int W_out = ydims.width - wdims.width + 1;
+  int mtx_numRow = ydims.depth;
+  int mtx_numCol = H_out * W_out;
+  // printf("[Test] wdims.height = %d, wdims.width = %d\n", wdims.height, wdims.width);
+  dim3 dimGrid(ceil((float) mtx_numRow/TILE_SZ_A), ceil((float) mtx_numCol/TILE_SZ_B), ydims.num);
+  dim3 dimBlock(TILE_SZ_A, 1, 1);
   conv_forward_opt_kernel<<<dimGrid, dimBlock>>>(X, xdims, W, wdims, Y, ydims);
   THROW_IF_ERROR(cudaGetLastError());
 
@@ -145,6 +247,7 @@ static int eval(const shape wDims, const shape xDims, bool doVerify) {
   timer_start("Copying inputs to the GPU.");
   THROW_IF_ERROR(cudaMemcpy(deviceW, hostW, wByteCount, cudaMemcpyDefault));
   THROW_IF_ERROR(cudaMemcpy(deviceX, hostX, xByteCount, cudaMemcpyDefault));
+  cudaMemcpyToSymbol(const_k, hostW, wByteCount);
   timer_stop();
 
   //////////////////////////////////////////
